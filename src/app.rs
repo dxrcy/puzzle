dd21e78583d52bec689588a02a9d26a6eebd09bb
darcy <acednes@gmail.com>
2023-10-01 21:53:36 +1100

    add keyboard support

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,8 @@
 use ggez::{
     event::{EventHandler, MouseButton},
     graphics::{self, DrawMode, DrawParam, Mesh, Rect, TextLayout},
-    mint::Point2,
-    Context, winit::event::VirtualKeyCode,
+    winit::event::VirtualKeyCode,
+    Context,
 };
 
 use crate::{
@@ -14,6 +14,7 @@ use crate::{
 pub struct App {
     grid: Grid,
     active_tile: Option<(usize, usize)>,
+    using_keyboard: bool,
 }
 
 impl App {
@@ -21,40 +22,46 @@ impl App {
         Self {
             grid: Grid::new(),
             active_tile: None,
+            using_keyboard: false,
         }
     }
 
-    pub fn reset(&mut self, ctx: &mut Context) {
+    fn reset(&mut self, ctx: &mut Context) {
         *self = Self::new(ctx)
+    }
+
+    fn navigate_active_tile(&mut self, dx: isize, dy: isize) {
+        let Some(active) = self.active_tile else {
+            self.active_tile = Some((0, 0));
+            return;
+        };
+
+        let x = active.0 as isize + dx;
+        let y = active.1 as isize + dy;
+
+        let range = 0..GRID_SIZE as isize;
+
+        if range.contains(&x) && range.contains(&y) {
+            self.active_tile = Some((x as usize, y as usize))
+        };
+    }
+
+    fn move_tile(&mut self) {
+        if let Some((x, y)) = self.active_tile {
+            self.grid.shift_tiles(x, y);
+        }
     }
 }
 
 impl EventHandler for App {
     fn update(&mut self, ctx: &mut ggez::Context) -> Result<(), ggez::GameError> {
-        let Point2 { x, y } = ctx.mouse.position();
-
-        let (x, y) = (x / TILE_SIZE, y / TILE_SIZE);
-
-        let range = 0.0..GRID_SIZE as f32;
-        self.active_tile = if range.contains(&x) && range.contains(&y) {
-            Some((x as usize, y as usize))
-        } else {
-            None
-        };
-
         if ctx.mouse.button_just_pressed(MouseButton::Left) {
-            if let Some((x, y)) = self.active_tile {
-                self.grid.shift_tiles(x, y);
-            }
+            self.move_tile();
         }
 
         if self.grid.is_complete() {
             println!("Well done!");
             std::process::exit(0)
-        }
-
-        if ctx.keyboard.is_key_just_pressed(VirtualKeyCode::R) {
-            self.reset(ctx);
         }
 
         Ok(())
@@ -121,8 +128,76 @@ impl EventHandler for App {
                 )?;
                 canvas.draw(&mesh, DrawParam::default());
             }
+
+            if self.using_keyboard {
+                let rect = Rect::new(
+                    (x as f32 + PADDING + MARGIN) * SIZE,
+                    (y as f32 + PADDING + MARGIN) * SIZE,
+                    (1.0 - PADDING * 2.0) * SIZE,
+                    (1.0 - PADDING * 2.0) * SIZE,
+                );
+
+                let mesh = Mesh::new_rounded_rectangle(
+                    ctx,
+                    DrawMode::stroke(0.05 * SIZE),
+                    rect,
+                    0.1 * SIZE,
+                    color::TILE_ACTIVE_KEYS,
+                )?;
+                canvas.draw(&mesh, DrawParam::default());
+            }
         }
 
         canvas.finish(ctx)
+    }
+
+    fn mouse_motion_event(
+        &mut self,
+        _ctx: &mut Context,
+        x: f32,
+        y: f32,
+        _dx: f32,
+        _dy: f32,
+    ) -> Result<(), ggez::GameError> {
+        self.using_keyboard = false;
+
+        let (x, y) = (x / TILE_SIZE, y / TILE_SIZE);
+
+        let range = 0.0..GRID_SIZE as f32;
+        self.active_tile = if range.contains(&x) && range.contains(&y) {
+            Some((x as usize, y as usize))
+        } else {
+            None
+        };
+
+        Ok(())
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        input: ggez::input::keyboard::KeyInput,
+        _repeated: bool,
+    ) -> Result<(), ggez::GameError> {
+        self.using_keyboard = true;
+
+        use VirtualKeyCode as Key;
+        match input.keycode {
+            // Reset game
+            Some(Key::R) => self.reset(ctx),
+
+            // Move tiles
+            Some(Key::Space | Key::Return) => self.move_tile(),
+
+            // Navigate
+            Some(Key::Left | Key::H | Key::A) => self.navigate_active_tile(-1, 0),
+            Some(Key::Down | Key::J | Key::S) => self.navigate_active_tile(0, 1),
+            Some(Key::Up | Key::K | Key::W) => self.navigate_active_tile(0, -1),
+            Some(Key::Right | Key::L | Key::D) => self.navigate_active_tile(1, 0),
+
+            _ => (),
+        }
+
+        Ok(())
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -15,10 +15,11 @@ pub const SIZE: f32 = TILE_SIZE * (GRID_SIZE as f32 - MARGIN * 2.0) / GRID_SIZE 
 
 mod color {
     colors!(
-        BACKGROUND  (0x501616)
-        TILE        (0xffefe4)
-        TILE_ACTIVE (0xe5d0c2)
-        TEXT        (0xb69587)
-        EMPTY_CELL  (0x471414)
+        BACKGROUND       (0x501616)
+        TILE             (0xffefe4)
+        TILE_ACTIVE      (0xe5d0c2)
+        TILE_ACTIVE_KEYS (0xbbbbff)
+        TEXT             (0xb69587)
+        EMPTY_CELL       (0x471414)
     );
 }
